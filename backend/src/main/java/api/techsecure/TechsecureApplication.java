4ed17117fcad5c168cc1d24a4b4f36029c34598e
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-06-28 00:27:01 +0100

    Teck secure project setup

---
+++ backend/src/main/java/api/techsecure/TechsecureApplication.java
@@ -0,0 +1,13 @@
+package api.techsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsecureApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/api/techsecure/TechsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package api.techsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
