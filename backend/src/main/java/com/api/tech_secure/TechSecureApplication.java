b375eae80128cece648eeef4af631522f0274193
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-10-08 21:13:58 +0100

    feat: Init Spring Boot backend project
    Initial setup with Spring Initializer
    - Basic config & dependencies
    - Web, JPA, MySQL Driver, DevTools
    - Java 17, Spring Boot 3.3.4

---
+++ backend/src/main/java/com/api/tech_secure/TechSecureApplication.java
@@ -0,0 +1,13 @@
+package com.api.tech_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSecureApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/api/tech_secure/TechSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tech_secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
